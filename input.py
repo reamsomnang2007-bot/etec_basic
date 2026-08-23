99ba8bcc2e27304227cf4f95b81910611a9dec3d
Ream Somnang <reamsomnang2007@gmail.com>
2026-08-23 08:38:58 +0700

    input

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+s1 = int(input("Enter score 1: "))
+s2 = int(input("Enter score 2: "))
+total = s1 + s2
+print(f"Name: {name}")
+print(f"Score 1: {s1}")
+print(f"Score 2: {s2}")
+print(f"Total: {total}")
